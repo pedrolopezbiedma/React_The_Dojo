82f4927a06327a65e57c591054a2eb4040e2eaee
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-02-05 17:43:14 +0100

    Create the project creation form

--- src/pages/CreateProject/CreateProject.jsx
+++ src/pages/CreateProject/CreateProject.jsx
@@ -1,5 +1,106 @@
+// React
+import { useState } from "react";
+import Select from "react-select";
+
+// Styles
+import "./CreateProject.css";
+
+// Hooks
+import { useFirestoreGet } from "../../hooks/useFirestoreGet";
+
+// Categories
+const categories = [
+  { value: "development", label: "Development" },
+  { value: "design", label: "Design" },
+  { value: "sales", label: "Sales" },
+  { value: "marketing", label: "Marketing" },
+];
+
 const CreateProject = () => {
-  return <div className="create-project">Create Project</div>;
+  const { documents: users } = useFirestoreGet("users");
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState(null);
+  const [assignedUsers, setAssignedUsers] = useState([]);
+  const [formError, setFormError] = useState(null);
+
+  const usersOptions = users.map((user) => {
+    return { value: user, label: user.displayName };
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setFormError(null);
+    if (!category) {
+      setFormError("You need to assign a category");
+      return;
+    }
+
+    if (assignedUsers.length === 0) {
+      setFormError("You need to assign at least one user");
+      return;
+    }
+
+    console.log(
+      "Values in the form are >>",
+      name,
+      details,
+      dueDate,
+      category,
+      assignedUsers
+    );
+  };
+
+  return (
+    <div className="create-form">
+      <h2 className="page-title">Create a New Project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            required
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <label>
+          <textarea
+            required
+            type="text"
+            value={details}
+            onChange={(event) => setDetails(event.target.value)}
+          />
+        </label>
+        <label>
+          <input
+            required
+            type="date"
+            value={dueDate}
+            onChange={(event) => setDueDate(event.target.value)}
+          />
+        </label>
+        <label>
+          <Select
+            value={category}
+            options={categories}
+            value={category}
+            onChange={(event) => setCategory(event)}
+          />
+        </label>
+        <label>
+          <Select
+            isMulti
+            options={usersOptions}
+            onChange={(event) => setAssignedUsers([...event])}
+          />
+        </label>
+        <button className="btn">Create Project</button>
+        {formError && <div className="error">{formError}</div>}
+      </form>
+    </div>
+  );
 };
 
 export default CreateProject;
